b0cbc5053ea0de73a941d47afb6f3002fad658d9
Vlad Holo <holovlad@yandex.ru>
2022-06-21 22:16:39 +1000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  const firstText = 'Hello';
+  return (
+    <>
+      <div>{firstText.toLocaleUpperCase()}</div>
+      <h3>World</h3>
+    </>
+  );
+}
+
+export default App;
